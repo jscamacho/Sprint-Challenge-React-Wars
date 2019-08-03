ba47e9a61bdf9a958494d0bb898eb693541c0a06
Jason Camacho <jasoncamacho@Jasons-MacBook-Pro.local>
2019-08-02 23:06:34 -0500

    Sprint React Wars

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,7 +1,25 @@
 import React from 'react';
-import './App.css';
+import {useEffect, useState} from 'react';
+import StarWarsCharacter from './components/StarWarsCharacter';
+import axios from 'axios';
+import styled from 'styled-components';
+
+
 
 const App = () => {
+  const[file, setFile] = useState();
+  
+  useEffect(() => {
+    axios.get('https://swapi.co/api/people')
+    .then((info)=>{
+      setFile(info.data);
+    })
+    .catch(error => {
+      console.log('major error', error);
+    })
+  }, []);
+
+  let StarWarsCharacter = null;
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
 
@@ -12,6 +30,8 @@ const App = () => {
   return (
     <div className="App">
       <h1 className="Header">React Wars</h1>
+      <StarWarsCharacter
+      StarWarsCharacter = {file.StarWarsCharacter}/>
     </div>
   );
 }
